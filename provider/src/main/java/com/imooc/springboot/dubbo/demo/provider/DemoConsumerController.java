0f2dac2f6c8bc2f46bdefcdaac8133e01aa8abb6
chuan0818 <1785802229@qq.com>
2020-12-22 21:05:24 +0800

    初步完成dubbo调用

--- provider/src/main/java/com/imooc/springboot/dubbo/demo/provider/DemoConsumerController.java
+++ provider/src/main/java/com/imooc/springboot/dubbo/demo/provider/DemoConsumerController.java
@@ -1,14 +1,14 @@
-package com.imooc.springboot.dubbo.demo.consumer;
+package com.imooc.springboot.dubbo.demo.provider;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.imooc.springboot.dubbo.demo.DemoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * http://127.0.0.1:8100/sayHello?name=xxxx
+ * http://127.0.0.1:8089/sayHello?name=xxxx
+ * 既是dubbo服务消费者也是提供者
  */
 @RestController
 public class DemoConsumerController {
